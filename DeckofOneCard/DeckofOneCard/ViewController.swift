a1247a92f17897b39727e5510639829a710882a1
statusqua <andriyfilenko@gmail.com>
2019-01-10 15:59:45 -0700

    adds a bunch of stuff

--- DeckofOneCard/DeckofOneCard/ViewController.swift
+++ DeckofOneCard/DeckofOneCard/ViewController.swift
@@ -13,16 +13,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var strikesLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var highscoreLabel: UILabel!
-    
-    
-    
     @IBOutlet weak var drawCardButton: UIButton!
     @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var guessLabel: UILabel!
+    
+    var strikes: Int = 0
+    var score: Int = 0
+    var highscore: Int = 0
+    var guess: Bool = false
+    var newCardValue: String?
+    var currentCardValue: String? {
+        didSet {
+            previousCardValue = oldValue
+        }
+    }
+    var previousCardValue: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         drawDard()
         designDrawButton()
+    }
+    
+    @IBAction func drawCardButtonTapped(_ sender: Any) {
+        drawDard()
+    }
+    
+    @IBAction func lowerButtonTapped(_ sender: UIButton) {
+        guard let cardValue = currentCardValue else { print(">>>>>>>\(#file) \(#function) \(#line): guard let failed <<<<<<<"); return }
+        lower(cardValue: cardValue)
+        updateViews()
+    }
+    
+    @IBAction func higherButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
     }
     
     func designDrawButton() {
@@ -34,36 +60,45 @@ class ViewController: UIViewController {
         drawCardButton.setTitleColor(.white, for: .normal)
         //        drawCardButton.setImage(UIImage(named: "Wood"), for: .normal)
         drawCardButton.clipsToBounds = true
-        
-        
     }
     
     func drawDard() {
-        //        let card = CardController.drawCard()
-        //        let image = CardController.getImage(card!.image)
-        //        cardImageView.image = image
-        
-        CardController.drawCard(numberOfCards: 1) { (cards) in
+        CardController.fetchCard(numberOfCards: 1) { (cards) in
             let card = cards[0]
-            CardController.image(forURL: card.image, completion: { (cardImage) in
+            self.currentCardValue = card.value
+            CardController.fetchImage(forURL: card.image, completion: { (cardImage) in
                 guard let image = cardImage else { return }
-                self.cardImageView.image = image
+                DispatchQueue.main.async {
+                    self.cardImageView.image = image
+                }
             })
         }
     }
     
-    @IBAction func drawCardButtonTapped(_ sender: Any) {
+    func lower(cardValue: String) {
         drawDard()
+        guard let currentValue = currentCardValue,
+            let previousValue = previousCardValue,
+            let current = CardScores.CardValues[currentValue],
+            let previous = CardScores.CardValues[previousValue]
+            else { print(">>>>>>>\(#file) \(#function) \(#line): guard let failed <<<<<<<"); return }
+        if current < previous {
+            guessLabel.text = "Good Guess"
+            score += 1
+        } else if current > previous {
+            strikes += 1
+        }
     }
     
-    @IBAction func lowerButtonTapped(_ sender: UIButton) {
+    func updateViews() {
+        scoreLabel.text = String(score)
+        strikesLabel.text = String(strikes)
     }
-    
-    @IBAction func higherButtonTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func resetButtonTapped(_ sender: UIButton) {
-    }
+        
 }
 
+
+//        let card = CardController.drawCard()
+//        let image = CardController.getImage(card!.image)
+//        cardImageView.image = image
 
