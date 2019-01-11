c0bc8ee31a788dccea98a25744306902bcf94505
statusqua <andriyfilenko@gmail.com>
2019-01-10 17:04:24 -0700

    adds CardScores

---
+++ DeckofOneCard/DeckofOneCard/Model/View Controller/CardScores.swift
@@ -0,0 +1,27 @@
+//
+//  CardScores.swift
+//  DeckofOneCard
+//
+//  Created by Drew on 1/10/19.
+//  Copyright © 2019 Jack Knight. All rights reserved.
+//
+import Foundation
+
+class CardScores {
+    
+    static let CardValues: [String: Int] = [
+        "ACE": 14,
+        "KING": 13,
+        "QUEEN": 12,
+        "JACK": 11,
+        "10": 10,
+        "9": 9,
+        "8": 8,
+        "7": 7,
+        "6": 6,
+        "5": 5,
+        "4": 4,
+        "3": 3,
+        "2" : 2
+    ]
+}
